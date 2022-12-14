05df145b29bb0d487f652af4f45a1ef1f6dfbee8
George Wortmann <gf.wortmann@gmail.com>
2022-12-15 02:08:32 +0300

    task 68 1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
-﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-//M = 1; N = 15 -> 120
-//M = 4; N = 8. -> 30
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
 
-int SumNaturalNumbers (int M, int N)
+
+int AckermanFunction( int M, int N)
 {
-    if (M <= N) return (SumNaturalNumbers (M+1, N) + M);
-    else return 0;
+    if (M == 0)                     return N + 1;
+    else if ((M > 0) && (N == 0))   return AckermanFunction ( M - 1, 1 );
+    else if ((M > 0) && (N > 0))    return AckermanFunction ( M - 1, AckermanFunction ( M, N - 1 ));
+    else                            return N + 1;
 }
 
-Console.Clear();
-int M1 = 1;
-int N1 = 15;
-Console.WriteLine($"\nSum of numbers from {M1} to {N1} is: {SumNaturalNumbers (M1, N1)}");
-
-int M2 = 4;
-int N2 = 8;
-Console.WriteLine($"\nSum of numbers from {M2} to {N2} is: {SumNaturalNumbers (M2, N2)}");
+Console.WriteLine ($" Ackerman Function A(2, 3) is: {AckermanFunction (2, 3)}");
+Console.WriteLine ($" Ackerman Function A(3, 2) is: {AckermanFunction (3, 2)}");
+Console.WriteLine ($" Ackerman Function A(3, 3) is: {AckermanFunction (3, 3)}");
+Console.WriteLine ($" Ackerman Function A(3, 4) is: {AckermanFunction (3, 4)}");
